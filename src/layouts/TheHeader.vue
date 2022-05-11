5ab8770a0aaa93e5bcc3305a846f4e617c95ac46
gymcoding <bruce.lean17@gmail.com>
2022-05-11 17:24:39 +0900

    3_the_header

--- src/layouts/TheHeader.vue
+++ src/layouts/TheHeader.vue
@@ -26,24 +26,30 @@
 								>About</RouterLink
 							>
 						</li>
+						<li class="nav-item">
+							<RouterLink class="nav-link" active-class="active" to="/posts">
+								게시글
+							</RouterLink>
+						</li>
 					</ul>
-					<form class="d-flex">
-						<input
-							class="form-control me-2"
-							type="search"
-							placeholder="Search"
-							aria-label="Search"
-						/>
-						<button class="btn btn-outline-success" type="submit">
-							Search
+					<div class="d-flex">
+						<button class="btn btn-outline-light" type="button" @click="goPage">
+							글쓰기
 						</button>
-					</form>
+					</div>
 				</div>
 			</div>
 		</nav>
 	</header>
 </template>
 
-<script setup></script>
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goPage = () => {
+	router.push('/posts/create');
+};
+</script>
 
 <style lang="scss" scoped></style>
