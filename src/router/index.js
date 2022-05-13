e1e74652dd0fa0abc111caf5716f4c9c8445207c
gymcoding <bruce.lean17@gmail.com>
2022-05-13 16:27:20 +0900

    6_history_mode_2

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,4 @@
-import {
-	createRouter,
-	createWebHashHistory,
-	createWebHistory,
-} from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import AboutView from '@/views/AboutView.vue';
 import PostCreateView from '@/views/posts/PostCreateView.vue';
