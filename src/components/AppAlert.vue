3821486010f72de4ef08dca7542fba7a48a1734d
gymcoding <bruce.lean17@gmail.com>
2022-05-16 17:21:50 +0900

    12_alert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,36 @@
+<template>
+	<div v-if="show" class="app-alert alert" :class="styleClass" role="alert">
+		{{ message }}
+	</div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+	show: {
+		type: Boolean,
+		default: false,
+	},
+	message: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		default: 'error',
+		validator: value => ['success', 'error'].includes(value),
+	},
+});
+const styleClass = computed(() =>
+	props.type === 'error' ? 'alert-danger' : 'alert-primary',
+);
+</script>
+
+<style scoped>
+.app-alert {
+	position: fixed;
+	top: 10px;
+	right: 10px;
+}
+</style>
--- src/views/posts/PostEditView.vue
+++ src/views/posts/PostEditView.vue
@@ -18,6 +18,7 @@
 				<button class="btn btn-primary">수정</button>
 			</template>
 		</PostForm>
+		<AppAlert :show="showAlert" :message="alertMessage" :type="alertType" />
 	</div>
 </template>
 
@@ -26,6 +27,7 @@ import { ref } from 'vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getPostById, updatePost } from '@/api/posts';
 import PostForm from '@/components/posts/PostForm.vue';
+import AppAlert from '@/components/AppAlert.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -41,6 +43,7 @@ const fetchPost = async () => {
 		setForm(data);
 	} catch (error) {
 		console.error(error);
+		vAlert('네트워크 오류!');
 	}
 };
 const setForm = ({ title, content }) => {
@@ -51,13 +54,27 @@ fetchPost();
 const edit = async () => {
 	try {
 		await updatePost(id, { ...form.value });
-		router.push({ name: 'PostDetail', params: { id } });
+		// router.push({ name: 'PostDetail', params: { id } });
+		vAlert('수정이 완료되었습니다!!!', 'success');
 	} catch (error) {
 		console.error(error);
 	}
 };
 
 const goDetailPage = () => router.push({ name: 'PostDetail', params: { id } });
+
+// alert
+const showAlert = ref(false);
+const alertMessage = ref('');
+const alertType = ref('');
+const vAlert = (message, type = 'error') => {
+	showAlert.value = true;
+	alertMessage.value = message;
+	alertType.value = type;
+	setTimeout(() => {
+		showAlert.value = false;
+	}, 2000);
+};
 </script>
 
 <style lang="scss" scoped></style>
