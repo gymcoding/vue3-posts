5e20d9e6c803b0614544b0d8d756203b14dabbc8
gymcoding <bruce.lean17@gmail.com>
2022-11-28 23:19:19 +0900

    update PostFilter

--- src/components/posts/PostFilter.vue
+++ src/components/posts/PostFilter.vue
@@ -26,15 +26,23 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 defineProps({
 	title: String,
 	limit: [Number, String],
 });
 const emit = defineEmits(['update:title', 'update:limit']);
 
+const valid = ref(true);
 const changeTitle = e => {
+	if (valid.value === false) {
+		return;
+	}
+	valid.value = false;
 	setTimeout(() => {
 		emit('update:title', e.target.value);
+		valid.value = true;
 	}, 500);
 };
 </script>
